fdb525ed2c2b622e7098d6ea781ac13eeef7bafc
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-05-02 12:41:36 -0400

    Disable Quagga on scanner unmount

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 import Scanner from './components/scanner'
 
 function App() {
-  const [scanning, setScanning] = useState(false);
+  const [scanner, setScanner] = useState({scanning: false, result: ''});
 
+  
   return (
     <div>
       <h1>Vegan Checker</h1>
-      <button onClick={() => {setScanning(!scanning)}}>turn {scanning? "off" : "on"} camera</button>
-      {scanning? <Scanner setScanning={setScanning}/> : null}
+      <button onClick={() => {setScanner({scanning: !scanner.scanning})}}>turn {scanner.scanning? "off" : "on"} camera</button>
+      {scanner.scanning? <Scanner scanner={scanner} setScanner={setScanner}/> : null}
     </div>
   );
 }
--- client/src/components/scanner.js
+++ client/src/components/scanner.js
@@ -38,6 +38,7 @@ export default class Scanner extends Component {
 
     componentWillUnmount() {
         Quagga.offDetected(this.detected);
+        Quagga.stop();
     }
 
     render(){
