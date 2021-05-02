e948881ab55c07a2683cd7e93a7bef928bae8c5a
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-05-01 23:50:41 -0400

    Add camera functionality

--- client/src/components/scanner.js
+++ client/src/components/scanner.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import Quagga from 'quagga';
 import "./components.css"
 
 export default class Scanner extends Component {
+    componentDidMount() {
+        Quagga.init({
+            inputStream: {
+                type : "LiveStream",
+                constraints: {
+                    width: 640,
+                    height: 480,
+                    facing: "environment"
+                }
+            },
+            locator: {
+                patchSize: "medium",
+                halfSample: true
+            },
+            numOfWorkers: 2,
+            decoder: {
+                readers : ["upc_reader"]
+            },
+            locate: true
+        }, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            Quagga.start();
+        });
+    }
+
     render(){
         return (
             <div>
                 this is where the scanner will appear
+                <div id="interactive" className="viewport"/>
             </div>
         )
     }
