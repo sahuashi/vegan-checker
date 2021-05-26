2e9f30641405f3098c66d8af03f0da80997b15ec
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-05-26 13:38:15 -0400

    Display result clientside

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 export default class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: ''
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/", {
+      params: {
+        upc: this.props.barcode
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          result: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
-    return <div> Hi </div>;
+    return (
+      <div>
+        {this.state.result ? <h1>{this.state.result}</h1> : null}
+      </div>
+    );
   }
 }
