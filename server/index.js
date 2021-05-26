6654940c4b2dcdd8caa804085625731f57c806aa
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-05-26 01:19:45 -0400

    Get nutrition from API

--- server/index.js
+++ server/index.js
@@ -11,27 +11,38 @@ const port = process.env.PORT || 5000;
 const id = process.env.APP_ID;
 const key = process.env.APP_KEY;
 
-const lookup = async() => {
-	try {
-		const response = await got(`https://api.edamam.com/api/food-database/v2/parser?upc=012000001291&app_id=${id}&app_key=${key}`);
-		console.log(response.body);
-	} catch (error) {
-		console.log(error.response.body);
-	}
-};
-
 app.get("/", (req, res) => {
-    lookup();
-    got.get(`https://api.edamam.com/api/food-database/v2/parser?upc=012000001291&app_id=${id}&app_key=${key}`, {responseType: 'json'})
+    got.get(`https://api.edamam.com/api/food-database/v2/parser?upc=012000001291&app_id=${id}&app_key=${key}`, {
+            responseType: 'json'
+        })
         .then(res => {
-            const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-            console.log('Status Code:', res.statusCode);
-            console.log('Date in Response header:', headerDate);
             console.log(JSON.stringify(res.body));
+            getNutrition(res.body.hints[0].food.foodId);
         })
         .catch(err => {
             console.log('Error: ', err.message);
         });
 })
 
-app.listen(port, () => {console.log(`Server is running on port ${port}!`)})
+const getNutrition = (food) => {
+    got.post(`https://api.edamam.com/api/food-database/v2/nutrients?app_id=${id}&app_key=${key}`, {
+            json: {
+                "ingredients": [{
+                    "quantity": 1,
+                    "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_serving",
+                    "foodId": food
+                }]
+            },
+            responseType: 'json'
+        })
+        .then(res => {
+            console.log(res.body);
+        })
+        .catch(err => {
+            console.log('Error: ', err.message);
+        });
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}!`)
+})
